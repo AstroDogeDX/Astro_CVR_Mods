8f3a99975436034fdbdf54efef212545046f1036
AstroDogeDX <48888500+AstroDogeDX@users.noreply.github.com>
2023-05-20 16:37:23 +0100

    [TurnKeys] Added prefs to adjust sensitivity

--- TurnKeys/Main.cs
+++ TurnKeys/Main.cs
@@ -2,33 +2,39 @@
 using HarmonyLib;
 using ABI_RC.Core.Savior;
 using UnityEngine;
+using Astro.TurnKeys.Properties;
 
-namespace Astro.TurnKeys
+namespace Astro.TurnKeys;
+public class TurnKeys : MelonMod
 {
-    public class TurnKeys : MelonMod
+    private MelonPreferences_Category mainCategory;
+    private MelonPreferences_Entry<float> turnSensitivity;
+    public override void OnInitializeMelon()
     {
-        public override void OnInitializeMelon()
-        {
-            MelonLogger.Msg("Loaded!");
-        }
+        mainCategory = MelonPreferences.CreateCategory("TurnKeys");
+        turnSensitivity = mainCategory.CreateEntry<float>("Turn Sensitivity", 35f,
+            description: "How much per frame you turn while holding the key down.");
+        MelonLogger.Msg($"Loaded! {AssemblyInfoParams.Version}");
+
+        HarmonyPatches.__TurnKeys = this;
     }
 
     [HarmonyPatch]
     public class HarmonyPatches
     {
+        public static TurnKeys __TurnKeys;
+
         [HarmonyPostfix]
         [HarmonyPatch(typeof(InputModuleMouseKeyboard), nameof(InputModuleMouseKeyboard.UpdateInput))]
         public static void after_InputModuleMouseKeyboard(InputModuleMouseKeyboard __instance)
         {
-            if (Input.GetKey(KeyCode.LeftBracket))
+            if (Input.GetKey(KeyCode.Q))
             {
-                MelonLogger.Msg("[ key pressed!");
-                __instance._inputManager.lookVector -= new Vector2(1f, 0f);
+                __instance._inputManager.lookVector -= new Vector2(__TurnKeys.turnSensitivity.Value, 0f) * Time.deltaTime;
             }
-            if (Input.GetKey(KeyCode.RightBracket))
+            if (Input.GetKey(KeyCode.E))
             {
-                MelonLogger.Msg("] key pressed!");
-                __instance._inputManager.lookVector += new Vector2(1f, 0f);
+                __instance._inputManager.lookVector += new Vector2(__TurnKeys.turnSensitivity.Value, 0f) * Time.deltaTime;
             }
         }
     }
