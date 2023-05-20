309e41dcf480f5310d98c2db1843adbc2b8b9997
AstroDogeDX <48888500+AstroDogeDX@users.noreply.github.com>
2023-05-20 15:44:54 +0100

    [TurnKeys] Initial control handling

--- TurnKeys/Main.cs
+++ TurnKeys/Main.cs
@@ -1,6 +1,35 @@
-﻿namespace Astro.TurnKeys
+﻿using MelonLoader;
+using HarmonyLib;
+using ABI_RC.Core.Savior;
+using UnityEngine;
+
+namespace Astro.TurnKeys
 {
-    public class TurnKeys
+    public class TurnKeys : MelonMod
     {
+        public override void OnInitializeMelon()
+        {
+            MelonLogger.Msg("Loaded!");
+        }
+    }
+
+    [HarmonyPatch]
+    public class HarmonyPatches
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(InputModuleMouseKeyboard), nameof(InputModuleMouseKeyboard.UpdateInput))]
+        public static void after_InputModuleMouseKeyboard(InputModuleMouseKeyboard __instance)
+        {
+            if (Input.GetKey(KeyCode.LeftBracket))
+            {
+                MelonLogger.Msg("[ key pressed!");
+                __instance._inputManager.lookVector -= new Vector2(1f, 0f);
+            }
+            if (Input.GetKey(KeyCode.RightBracket))
+            {
+                MelonLogger.Msg("] key pressed!");
+                __instance._inputManager.lookVector += new Vector2(1f, 0f);
+            }
+        }
     }
 }
